54c87becc53855a36a46a0290f17bd36ac5cba90
Marcin Siedlarz <marcin.siedlarz@gmail.com>
2018-08-13 23:04:37 +0200

    boilerplate

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Movie
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'genre', 'imdb_rating')
+    readonly_fields = ('title',)

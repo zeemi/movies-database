096820051dfcd53b643c4a3865fbf4add2238f33
Marcin Siedlarz <marcin.siedlarz@gmail.com>
2018-08-14 17:35:03 +0200

    open movies db wrapper added

--- movies/urls.py
+++ movies/urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+
+from django.urls import path
+
+from .views import MovieViewSet
 
 urlpatterns = [
-    path('movies/', include('movies.urls')),
-
-    path('admin/', admin.site.urls),
+    path(r'', MovieViewSet.as_view({'post': 'create'}), name='add-movie'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
-# Create your views here.
+from libs.omdbapi_wrapper import OpenMovieDatabase
+from .serializers import MovieSerializer, MovieAddSerializer
+from .models import Movie
+
+
+class MovieViewSet(mixins.CreateModelMixin,
+                               viewsets.GenericViewSet):
+    """
+    A simple ViewSet for listing or retrieving users.
+    """
+    # def list(self, request):
+    #     queryset = Movie.objects.all()
+    #     serializer = UserSerializer(queryset, many=True)
+    #     return Response(serializer.data)
+
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+    permission_classes = (AllowAny,)
+
+    def create(self, request, **kwargs):
+        serializer = MovieAddSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        movie_definition = OpenMovieDatabase().get_by_title(serializer.validated_data['title'])
+        return super(MovieViewSet, self).create(request, **kwargs)
